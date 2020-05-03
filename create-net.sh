996ae25b5b28497333ec1ce8b2d6d4a7be7c35cb
Ubuntu <jcdiaztorres96@gmail.com>
2020-05-03 11:56:56 +0000

    sleeps added

--- create-net.sh
+++ create-net.sh
@@ -1,3 +1,6 @@
-sudo docker network create -d macvlan --subnet=172.30.2.0/24 --gateway=172.30.2.1 -o parent=enp0s3.100 physical
-#sudo docker network create -d macvlan --subnet=172.30.0.0/16 --gateway=172.30.0.1 -o parent=enp0s3.100 physical
+## Create ip allocation in 1st IP
+sudo ip link add link ens3 name ens3.100 type vlan id 100
+sudo ip addr add 172.30.2.1/24 brd 172.30.2.255 dev ens3.100
+sudo ip link set dev ens3.100 up
+sudo docker network create -d macvlan --subnet=172.30.2.0/24 --gateway=172.30.2.1 -o parent=ens3.100 physical
 
